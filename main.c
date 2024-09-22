9eaebc9d78d9197601ae26aaacfdfb462c9bff4c
Sevak <sevaksantrosyan5@gmail.com>
2024-09-22 22:37:40 +0400

    Operating Systems homework 4

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main() {
+    int number = 7;
+    int number_squared = squareMethod(number);
+    printf("Digit: %d, Square: %d", number, number_squared);
+    return 0;
+}
---
+++ math_utils.c
@@ -0,0 +1,5 @@
+#include "math_utils.h"
+
+int squareMethod(int number) {
+    return number * number;
+}
